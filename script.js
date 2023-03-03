6751f16cac6fb469d21c607104568a1d08722ca7
aj2000 <hi@ajaysharma.dev>
2023-03-03 23:25:41 +0530

    readme And boilerplate update

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+function calculateGrade(percentage) {
+  //your code here
+}
+const percentage = parseInt(prompt("Enter Percentage."));
+alert(calculateGrade(percentage));
